2bd5fd37d8ceea97954421aa74d22124bd907443
Alex_ru <barakudatm@gmail.com>
2017-05-04 16:50:36 +0300

    add files

---
+++ loger.js
@@ -0,0 +1,67 @@
+const colors = require('colors');
+const fs     = require('fs');
+
+module.exports = class Loger{
+    constructor (name, level) {
+        this.name = name;
+        this.messageLevel = level || 3;
+        this.gefaultLevel = 3;
+        this.logFilePath = './log/log.txt';
+        this.time = true;
+    }
+
+    log (message, level) {
+        level = level || this.gefaultLevel;
+        if( level <= this.messageLevel) {
+            let type = ' ';
+            let typeF = ' ';
+            let time = '';
+            let timeF = '';
+            let name = this.name.cyan;
+            let nameF = this.name;
+
+            switch (level) {
+                case 3:
+                    type = '[info] '.green;
+                    typeF = '[info] ';
+                break;
+                case 2:
+                    type = '[warn] '.yellow;
+                    typeF = '[warn] ';
+                break;
+                case 1:
+                    name = this.name.red;
+                    type = '[error] '.red;
+                    typeF = '[error] ';
+                break;
+            }
+
+            if ( this.time ) {
+                time = this.getTime().red;
+                timeF = this.getTime();
+            }
+            console.log(type + time + name + ':', message);
+            if (this.logFilePath) {
+                fs.appendFileSync(this.logFilePath, typeF + timeF + nameF + ': ' + message + "\r\n")
+            }
+        }
+    }
+
+    /**
+     * You can change the timeout setting
+     * @param value (boolean)
+     */
+    setTimeWrite ( value ) {
+        this.time = value;
+    }
+    getTime () {
+        let time = new Date();
+        return '[' + time.getHours() + ':' + time.getMinutes() + ':' +  time.getSeconds() + ' ' + time.getMilliseconds() + '] ';
+    }
+    setLevel (level) {
+        this.messageLevel = level;
+    }
+    setDefaultLevel (level) {
+        this.gefaultLevel = level;
+    }
+}
---
+++ server.js
@@ -0,0 +1,12 @@
+const includer  = require('./includer');
+const loger     = includer.set('Loger',     require('./loger'));
+const http      = includer.set('http',      require('http'));
+const colors    = includer.set('colors',    require('colors'));
+const config    = includer.set('config',    require('config'));
+const md5       = includer.set('md5',       require('md5'));
+const router    = includer.set('router',    require('router'));
+const validator = includer.set('validator', require('validator'));
+const ws        = includer.set('ws',        require('ws'));
+const bodyParser    = includer.set('bodyParser',    require('body-parser'));
+const finalhandler  = includer.set('finalhandler',  require('finalhandler'));
+const jsonWebToken  = includer.set('jsonWebToken',  require('json-web-token'));
